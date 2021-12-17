d30f466674067fd68129a140b4fa5c4f0d9b83c5
Duo Admin <75315647+duoadmin@users.noreply.github.com>
2021-12-17 17:09:37 +0530

    Create Duoc.sol

---
+++ Duoc.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+
+    constructor () public ERC20("Duocodies", "DUO") {
+        _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
+    }
+}
